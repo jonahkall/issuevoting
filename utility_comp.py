6cb9e04f861b6ada8d929a705479e60c7b98b31b
Jonah Kallenbach <kallenbach@college.harvard.edu>
2016-04-29 02:46:56 -0400

    general election results

--- utility_comp.py
+++ utility_comp.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-
 # Column spec for election data:
 # [0: unique voter id]
 # [1:vote, 1 obama 2 romney]
@@ -21,19 +17,17 @@ import sys
 primary_data = open('jan12_new.csv')
 election_data = open('sept12_new.csv')
 
-primary_votes = []
-i = 0
-for l in primary_data:
-	if i != 0:
-		primary_votes.append(map(lambda x: int(x.rstrip()), l.split(",")))
-	i += 1
+def build_voting_data(file):
+	votes = []
+	i = 0
+	for l in file:
+		if i != 0:
+			votes.append(map(lambda x: int(x.rstrip()), l.split(",")))
+		i += 1
+	return votes
 
-election_votes = []
-i = 0
-for l in election_data:
-	if i != 0:
-		election_votes.append(map(lambda x: int(x.rstrip()), l.split(",")))
-	i += 1
+primary_votes = build_voting_data(primary_data)
+election_votes = build_voting_data(election_data)
 
 # Rank the primary candidates according to our rule and plurality voting.
 scores = [0] * 6
@@ -48,6 +42,7 @@ for vote in primary_votes:
 	for j in xrange(len(vote[6:10])):
 		scores[vote[6 + j] - 1] += alpha[j]
 
+print "\n***PRIMARY***"
 print "According to our voting rule:"
 cand_dict = {1:'Gingrich',2:'Huntsman',3:'Paul',4:'Perry',5:'Romney',6:'Santorum'}
 for k in cand_dict.keys():
@@ -83,6 +78,53 @@ for vote in primary_votes:
 	total_utility_our_rule += utility_i_1
 	total_utility_plurality_rule += utility_i_plurality
 
-print "Total utility under our rule:", total_utility_our_rule
+print "\nTotal utility under our rule:", total_utility_our_rule
 print "Total utility under their rule:", total_utility_plurality_rule
+
+print "\n***GENERAL ELECTION***"
+
+# Rank the general election candidates according to our rule and plurality voting.
+# 0: obama, 1: romney
+ge_scores = [0,0]
+ge_plurality_scores = [0,0]
+
+for vote in election_votes:
+	if 9 in vote or 8 in vote or 7 in vote:
+		continue
+	ge_plurality_scores[vote[1]-1] += 1
+	alpha = vote[2:8]
+	for j in xrange(len(vote[8:14])):
+		ge_scores[2 - vote[8 + j]] += alpha[j]
+
+print "According to our voting rule:"
+cand_dict = {1:'Obama', 2:'Romney'}
+for k in cand_dict.keys():
+	print "Candidate " + cand_dict[k] + " received: " + str(ge_scores[k-1])
+print "\n"
+
+print "According to the plurality rule, the winners are:"
+for k in cand_dict.keys():
+	print "Candidate " + cand_dict[k] + " received: " + str(ge_plurality_scores[k-1])
+
+ge_total_utility_our_rule = 0
+ge_total_utility_plurality_rule = 0
+
+print ge_scores, ge_plurality_scores
+
+ge_gamma_1 = [float(k)/sum(ge_scores) for k in ge_scores]
+ge_gamma_plurality = [float(k)/sum(ge_plurality_scores) for k in ge_plurality_scores]
+
+print ge_gamma_plurality, ge_gamma_1
+
+for vote in election_votes:
+	if 9 in vote or 8 in vote or 7 in vote:
+		continue
+
+	alpha = map(float,vote[2:8])
+	for j in xrange(len(vote[8:14])):
+		ge_total_utility_our_rule += alpha[j] * ge_gamma_1[2 - vote[8 + j]]
+		ge_total_utility_plurality_rule += alpha[j] * ge_gamma_plurality[2 - vote[8 + j]]
+
+print "\nTotal utility under our rule:", ge_total_utility_our_rule
+print "Total utility under their rule:", ge_total_utility_plurality_rule
 
