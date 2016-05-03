8d401ad43633ff960803ba3079a36eec2df7272e
Jonah Kallenbach <kallenbach@college.harvard.edu>
2016-05-03 01:44:59 -0400

    two step aggro working

--- utility_comp.py
+++ utility_comp.py
@@ -1,3 +1,6 @@
+import numpy as np
+import sys
+
 # Column spec for election data:
 # [0: unique voter id]
 # [1:vote, 1 obama 2 romney]
@@ -14,7 +17,7 @@
 
 # Unpack and parse the data from the files contianing the primary and
 # election data.
-primary_data = open('jan12_new.csv')
+primary_data = open('blah2.csv')
 election_data = open('sept12_new.csv')
 
 def build_voting_data(file):
@@ -28,19 +31,22 @@ def build_voting_data(file):
 
 primary_votes = build_voting_data(primary_data)
 election_votes = build_voting_data(election_data)
-
+alphas = []
 # Rank the primary candidates according to our rule and plurality voting.
 scores = [0] * 6
 plurality_scores = [0] * 6
 for vote in primary_votes:
+	#print vote
 	if 9 in vote or 8 in vote or 7 in vote:
 		continue
 	plurality_scores[vote[1]-1] += 1
 
 	# Issue importances mapped to 0 - 3
-	alpha = map(lambda x: 4 - x, vote[2:6])
+	alpha = map(lambda x: 5 - x, vote[2:6])
+	alpha_norm = [float(a)/float(sum(alpha)) for a in alpha]
+	alphas.append(alpha_norm)
 	for j in xrange(len(vote[6:10])):
-		scores[vote[6 + j] - 1] += alpha[j]
+		scores[vote[6 + j] - 1] += alpha_norm[j]
 
 print "\n***PRIMARY***"
 print "According to our voting rule:"
@@ -61,8 +67,12 @@ for k in cand_dict.keys():
 total_utility_our_rule = 0
 total_utility_plurality_rule = 0
 
-gamma_1 = [float(k)/sum(scores) for k in scores]
-gamma_plurality = [float(k)/sum(plurality_scores) for k in plurality_scores]
+gamma_1 = np.argsort([float(k)/sum(scores) for k in scores])
+gamma_plurality = np.argsort([float(k)/sum(plurality_scores) for k in plurality_scores])
+
+print "gammas!"
+print gamma_1
+print gamma_plurality
 
 for vote in primary_votes:
 	if 9 in vote or 8 in vote or 7 in vote:
@@ -70,10 +80,12 @@ for vote in primary_votes:
 	utility_i_1 = 0
 	utility_i_plurality = 0
 
-	alpha = map(lambda x: 4 - x, vote[2:6])
+	alpha = map(lambda x: 5 - x, vote[2:6])
+	alpha_norm = [float(a)/float(sum(alpha)) for a in alpha]
+	#print alpha_norm
 	for j in xrange(len(vote[6:10])):
-		utility_i_1 += alpha[j] * gamma_1[vote[6 + j] - 1]
-		utility_i_plurality += alpha[j] * gamma_plurality[vote[6 + j] - 1]
+		utility_i_1 += alpha_norm[j] * gamma_1[vote[6 + j] - 1]
+		utility_i_plurality += alpha_norm[j] * gamma_plurality[vote[6 + j] - 1]
 
 	total_utility_our_rule += utility_i_1
 	total_utility_plurality_rule += utility_i_plurality
@@ -91,10 +103,12 @@ ge_plurality_scores = [0,0]
 for vote in election_votes:
 	if 9 in vote or 8 in vote or 7 in vote:
 		continue
+
 	ge_plurality_scores[vote[1]-1] += 1
 	alpha = vote[2:8]
+	alpha_norm = [float(a)/float(sum(alpha)) for a in alpha]
 	for j in xrange(len(vote[8:14])):
-		ge_scores[2 - vote[8 + j]] += alpha[j]
+		ge_scores[2 - vote[8 + j]] += alpha_norm[j]
 
 print "According to our voting rule:"
 cand_dict = {1:'Obama', 2:'Romney'}
@@ -111,8 +125,8 @@ ge_total_utility_plurality_rule = 0
 
 print ge_scores, ge_plurality_scores
 
-ge_gamma_1 = [float(k)/sum(ge_scores) for k in ge_scores]
-ge_gamma_plurality = [float(k)/sum(ge_plurality_scores) for k in ge_plurality_scores]
+ge_gamma_1 = np.argsort([float(k)/sum(ge_scores) for k in ge_scores])
+ge_gamma_plurality = np.argsort([float(k)/sum(ge_plurality_scores) for k in ge_plurality_scores])
 
 print ge_gamma_plurality, ge_gamma_1
 
@@ -121,10 +135,41 @@ for vote in election_votes:
 		continue
 
 	alpha = map(float,vote[2:8])
+	alpha_norm = [a/float(sum(alpha)) for a in alpha]
 	for j in xrange(len(vote[8:14])):
-		ge_total_utility_our_rule += alpha[j] * ge_gamma_1[2 - vote[8 + j]]
-		ge_total_utility_plurality_rule += alpha[j] * ge_gamma_plurality[2 - vote[8 + j]]
+		ge_total_utility_our_rule += alpha_norm[j] * ge_gamma_1[2 - vote[8 + j]]
+		ge_total_utility_plurality_rule += alpha_norm[j] * ge_gamma_plurality[2 - vote[8 + j]]
 
 print "\nTotal utility under our rule:", ge_total_utility_our_rule
 print "Total utility under their rule:", ge_total_utility_plurality_rule
+
+# Now we will attempt to perform two step aggregation for the primary data
+# First, we conduct the alpha election.  For now let's just take an average.
+alpha_final = [0] * len(alphas[0])
+for alph in alphas:
+	for j in xrange(len(alphas[0])):
+		alpha_final[j] += alph[j]
+
+for j in xrange(len(alphas[0])):
+	alpha_final[j] = (1.0/len(alphas)) * alpha_final[j]
+
+# 6 is num candidates, 4 is num issues
+primary_betas = np.zeros((6,4))
+
+# Now let's conduct the election for each beta
+for vote in primary_votes:
+	if 9 in vote or 8 in vote or 7 in vote:
+		continue
+	for j in xrange(len(vote[6:10])):
+		primary_betas[vote[6 + j] - 1][j] += 1
+
+two_step_aggro_scores = [0] * 6
+
+for j in xrange(4):
+	for i in xrange(6):
+		two_step_aggro_scores[i] += alpha_final[j] * primary_betas[i][j]
+
+print two_step_aggro_scores
+print np.argsort(two_step_aggro_scores)
+
 
